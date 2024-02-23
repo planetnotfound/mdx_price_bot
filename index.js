911c9a8504e1f593396c04adad4557ecbbf6587f
Jonathan Fritz <fritz@nosc.io>
2024-02-23 20:58:01 +0100

    add: simple mdx price bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const endpoint = "https://api.trade.mandala.exchange/api/3/public/price/ticker";
+const ticker_to_watch = "MDXUSDT"; // mdx to the moon!
+
+bot.on("message", async (ctx) => {
+	if (ctx.message.text === "/price") {
+		const response = await fetch(endpoint).then((response) => {
+			return response.json();
+		});
+		const mdx_ticket = response[ticker_to_watch];
+		ctx.reply(`MDX price is: ${mdx_ticket.price}`);
+	}
+});
+
+bot.launch();
