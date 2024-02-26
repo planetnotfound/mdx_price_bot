6c892ce4ac3612d638481a52332de5284f712169
Jonathan Fritz <fritz@nosc.io>
2024-02-26 10:38:35 +0100

    feat: displaying volume

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const endpoint = "https://api.trade.mandala.exchange/api/3/public/price/ticker";
+// const priceEndpoint =
+// 	"https://api.trade.mandala.exchange/api/3/public/price/ticker";
+const endpoint =
+	"https://api.trade.mandala.exchange/api/3/public/ticker/MDXUSDT";
 const ticker_to_watch = "MDXUSDT"; // mdx to the moon!
 
 let can_request_price = true;
 let cached_price = 0;
+let cached_volume_usdt = 0;
 /**
  * Returns the price but throttles the request to 1 request every 5 seconds
  * @returns
@@ -17,18 +21,20 @@ async function requestThePrice() {
 			const response = await fetch(endpoint).then((response) => {
 				return response.json();
 			});
-			const mdx_ticket = response[ticker_to_watch];
-			cached_price = mdx_ticket.price;
+			const mdx_ticker = response;
+			cached_price = mdx_ticker.last;
+			cached_volume_usdt = (mdx_ticker.volume * mdx_ticker.last).toFixed(2);
 			can_request_price = false;
 			setTimeout(() => {
 				can_request_price = true;
 			}, 5000);
-			return `The current price of ${ticker_to_watch} is ${mdx_ticket.price}`;
+			return `The current price of ${ticker_to_watch} is $${cached_price} with a volume of $${cached_volume_usdt}`;
 		} catch (error) {
+			console.error(error);
 			return "Error fetching price. Please try again later. We are already working on it!";
 		}
 	} else {
-		return `The current price of ${ticker_to_watch} is ${cached_price}`;
+		return `The current price of ${ticker_to_watch} is $${cached_price} with a volume of $${cached_volume_usdt}`;
 	}
 }
 
@@ -43,7 +49,7 @@ bot.on("message", async (ctx) => {
 	if (ctx.message.text === "/start") {
 		console.log("greeted", username);
 		ctx.reply(
-			"Welcome to the unofficial Mandala Exchange Bot! You can use /price to get the current price of MDXUSDT. This bot is not affiliated with Mandala Exchange and is being maintained by the community. If you have any questions or feedback, please reach out to @jan_may.",
+			"Welcome to the unofficial Mandala Exchange Price Bot! You can use /price to get the current price of MDXUSDT. This bot is not affiliated with Mandala Exchange and is being maintained by the community. If you have any questions or feedback, please reach out to @jan_may.",
 		);
 	}
 });
